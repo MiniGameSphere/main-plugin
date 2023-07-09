cce2b10f84932bd338a13d3c40763d18149d9024
FubyCutie <roset280905@gmail.com>
2023-07-09 03:01:21 +0100

    converted plugin to kotlin and org namespace

---
+++ src/main/kotlin/com/mgs/mainPlugin/Main.kt
@@ -0,0 +1,15 @@
+package com.mgs.mainPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+class Main : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+
+    }
+
+    override fun  onDisable() {
+        // Plugin shutdown logic
+    }
+}
